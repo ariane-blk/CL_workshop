4aeb1397809158e04cd4bc46684ad752e736ad4a
ariane <ariane.bollack@gmail.com>
2025-07-22 18:12:00 +0100

    add manifest.json file

--- global.R
+++ global.R
@@ -1,7 +1,7 @@
 # Welcome to the Centiloid workshop
 
 # INSTALL DEPENDENCIES ----------------------------------------------------
-setwd("~/Desktop/CL_workshop")
+#setwd("~/Desktop/CL_workshop")
 source('dependencies.R')
 # load all packages
 lapply(required_packages, require, character.only = TRUE)
@@ -13,5 +13,3 @@ software_clinical_table <- read_csv("data/software_clinical.csv") %>%
   select(-Website)
 software_research_table <- read_csv("data/software_research.csv") 
 h2h_table <- read_csv("data/h2h_f18_tracers.csv") 
-
-#rmarkdown::render("intro.Rmd", output_file = "intro.html", quiet = TRUE)
